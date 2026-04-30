1333048a90ab5c2cbe997ee700346f35dae89617
Anatnaso <antondev@kuthy.com>
2026-04-30 09:53:28 +0200

    Update `README.md`, `pyproject.toml` and configure entrypoint
    On branch master
    Changes to be committed:
    	modified:   README.md
    	modified:   pyproject.toml
    	modified:   src/freemedia/__init__.py
    	deleted:    src/main.py
    	new file:   uv.lock

---
+++ README.md
@@ -0,0 +1,2 @@
+# **Anatnaso's FreeMedia**
+Simple infrastructure for hosting different kinds of media.
--- src/main.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-def main():
-    print("Hello from freemedia!")
-
-
-if __name__ == "__main__":
-    main()
